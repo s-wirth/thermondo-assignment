55bb21ba174be4b6658a6da320f27cb520d4e446
Sophie <a.sophiewirth@gmail.com>
2015-03-16 22:29:18 +0100

    Create Update View

--- testAssigment/customers/urls.py
+++ testAssigment/customers/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
-from customers.views import CustomerList
-from customers.views import CreateCustomerView
+from customers.views import CustomerList, CreateCustomerView, CustomerUpdateView
 from . import views
 
 urlpatterns = patterns('',
     url(r'^$', CustomerList.as_view(), name="customer-list"),
     url(r'^create-customer$', CreateCustomerView.as_view(), name="create-customer"),
+    url(r'^customer-update/(?P<pk>\d+)$', CustomerUpdateView.as_view(), name="customer-update"),
 )
--- testAssigment/customers/views.py
+++ testAssigment/customers/views.py
@@ -3,6 +3,7 @@ from customers.models import Customer
 from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse
 from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView
 
 class CustomerList(ListView):
 
@@ -12,6 +13,14 @@ class CreateCustomerView(CreateView):
 
     model = Customer
     template_name = 'customers/create_customer.html'
+
+    def get_success_url(self):
+        return reverse('customer-list')
+
+class CustomerUpdateView(UpdateView):
+
+    model = Customer
+    template_name = 'customers/customer_update_form.html'
 
     def get_success_url(self):
         return reverse('customer-list')
