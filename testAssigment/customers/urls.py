39c5f4979272143012e5a22ca6d2e8adc24a062d
Sophie <a.sophiewirth@gmail.com>
2015-03-16 22:01:14 +0100

    Created “create new customer view”

--- testAssigment/customers/urls.py
+++ testAssigment/customers/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from customers.views import CustomerList
+from customers.views import CreateCustomerView
 from . import views
 
 urlpatterns = patterns('',
     url(r'^$', CustomerList.as_view(), name="customer-list"),
+    url(r'^create-customer$', CreateCustomerView.as_view(), name="create-customer"),
 )
--- testAssigment/customers/views.py
+++ testAssigment/customers/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from customers.models import Customer
 from django.views.generic.list import ListView
+from django.core.urlresolvers import reverse
+from django.views.generic import CreateView
 
 class CustomerList(ListView):
 
     model = Customer
+
+class CreateCustomerView(CreateView):
+
+    model = Customer
+    template_name = 'customers/create_customer.html'
+
+    def get_success_url(self):
+        return reverse('customer-list')
